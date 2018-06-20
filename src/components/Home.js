1ffa8913ba2214e556e530ac022cdfa441782dfd
Yaron <you@example.com>
2018-06-20 09:46:51 +0300

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -52,11 +52,11 @@ export default class Home extends Component {
           )
         )
       ]
-    ).catch(err => console.log(err));
+    ).catch(err => console.log(err) && <div>ERROR...</div>);
   }
   render() {
     if (Object.keys(this.state.daily).length === 0 || Object.keys(this.state.current).length === 0) {
-      return <div>ERROR...</div>;
+      return <div>LOADING...</div>;
     }
     console.log(this.state);
     return (
@@ -68,31 +68,35 @@ export default class Home extends Component {
         <div>
           <h4>Current Temp: {Math.floor(this.state.current.data[0].temp)}</h4>
         </div>
-        {this.state.daily.data.map((data, i) => (
-          <div key={i}>
-            <span>
-              <strong>Weather on</strong> {data.datetime}:{" "}
-            </span>
-            <span className="hourly-weather">{data.temp} </span>
-            <span>{data.weather.description} </span>
-            <span>
-              <strong>Min Temp:</strong> {data.min_temp}{" "}
-            </span>
-            <span>
-              <strong>Max Temp:</strong> {data.max_temp}{" "}
-            </span>
-            <div>
-              <Link
-                to={{
-                  pathname: `/hourly/${data.datetime}`,
-                  state: this.state.hourly
-                }}
-              >
-                <img src={`https://www.weatherbit.io/static/img/icons/${data.weather.icon}.png`} alt="_weather" />
-              </Link>
+        <div style={{ display: "flex" }}>
+          {this.state.daily.data.map((data, i) => (
+            <div key={i}>
+              <div>
+                <Link
+                  to={{
+                    pathname: `/hourly/${data.datetime}`,
+                    state: this.state.hourly
+                  }}
+                >
+                  <img src={`https://www.weatherbit.io/static/img/icons/${data.weather.icon}.png`} alt="_weather" />
+                </Link>
+              </div>
+              <div>
+                <span>
+                  <strong>Weather on</strong> {data.datetime}:{" "}
+                </span>
+                <span className="hourly-weather">{data.temp} </span>
+                <span>{data.weather.description} </span>
+                <span>
+                  <strong>Min Temp:</strong> {data.min_temp}{" "}
+                </span>
+                <span>
+                  <strong>Max Temp:</strong> {data.max_temp}{" "}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     );
   }
--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -3,18 +3,22 @@ import { Link } from "react-router-dom";
 
 export default props => {
   const getDayWeather = props.location.state.data.filter(date => date.datetime.split(":")[0] === props.match.params.date);
-  console.log(getDayWeather);
+  console.log(props);
   return (
     <div>
       <div>
         <Link to="/">Back Home</Link>
       </div>
-      <h2>Hourly Weather Forecast for {props.match.params.date}</h2>
+      <h2>
+        Hourly Weather Forecast in {props.location.state.city_name}, {props.location.state.country_code} on {props.match.params.date}
+      </h2>
       {getDayWeather.map((hourWeather, i) => (
         <div key={i}>
-          <span>{hourWeather.timestamp_local}: </span>
+          <div>
+            <img src={`https://www.weatherbit.io/static/img/icons/${hourWeather.weather.icon}.png`} alt="_weather" style={{ width: "40px" }} />
+          </div>
+          <span>{hourWeather.timestamp_local.split("T")[1]}: </span>
           <span>Temp: {hourWeather.temp} </span>
-          <img src={`https://www.weatherbit.io/static/img/icons/${hourWeather.weather.icon}.png`} alt="_weather" />
         </div>
       ))}
     </div>
